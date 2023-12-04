36c3d1ab1db0ae9f37078ffd481f31621cfba375
salixor <salixor@pm.me>
2023-12-04 17:51:15 +0100

    Day 4 part 2 in bash

--- 04/day4.sh
+++ 04/day4.sh
@@ -1,29 +1,41 @@
-line_regex='Card.*([[:digit:]]+): (.*) \| (.*)'
+line_regex="Card[[:blank:]]+([[:digit:]]+)\: (.*) \| (.*)"
 
 function compare_strings() {
     found_matches=0
-    while IFS=" "; read -ra numbers; do
-        for number in "${numbers[@]}"; do
-            regex="([[:blank:]]+|^)$number([[:blank:]]+|$)"
-            if [[ "$1" =~ $regex ]]; then
-                ((found_matches++))
-            fi
-        done
-    done <<< "$2"
+    numbers=($2)
+    for number in "${numbers[@]}"; do
+        regex="([[:blank:]]+|^)$number([[:blank:]]+|$)"
+        [[ "$1" =~ $regex ]] && ((found_matches++))
+    done
     echo "$found_matches"
 }
 
-function day4() {
+function day4_part1() {
     sum=0
     while read l; do
-        if [[ $l =~ $line_regex ]]; then
-            matches=$(compare_strings "${BASH_REMATCH[2]}" "${BASH_REMATCH[3]}")
-            if (( $matches > 0 )); then
-                sum=$(( $sum + 2 ** ($matches - 1) ))
-            fi
-        fi
+        [[ $l =~ $line_regex ]]
+        matches=$(compare_strings "${BASH_REMATCH[2]}" "${BASH_REMATCH[3]}")
+        (( $matches > 0 )) && ((sum+=2**($matches-1)))
     done <<< "$1"
     echo "$sum"
 }
 
-day4 "$1"
+function day4_part2() {
+    sum=0
+    instances=()
+    while read l; do
+        [[ $l =~ $line_regex ]]
+        current_game="${BASH_REMATCH[1]}"
+
+        ((sum+=${instances["$current_game"]:=1}))
+        matches=$(compare_strings "${BASH_REMATCH[2]}" "${BASH_REMATCH[3]}")
+
+        for (( i=$current_game+1; i <= $current_game + matches; i++ )); do
+            instances["$i"]=$(( ${instances[$i]:=1} + ${instances["$current_game"]} ))
+        done
+    done <<< "$1"
+    echo "$sum"
+}
+
+day4_part1 "$1"
+day4_part2 "$1"
--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Languages may be all over the place.
 - Day 1 : 2023-12-01 20:37
 - Day 2 : 2023-12-02 21:06
 - Day 3 : ?
-- Day 4 : Part1 = 2023-12-04 14:57
+- Day 4 : 2023-12-04 16:07
